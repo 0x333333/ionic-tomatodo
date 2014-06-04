4fe602b68be45ecde4f00465fb81d035b67f73bd
jesusjzp <jesusjzp@gmail.com>
2014-06-03 22:34:59 -0700

    Add pie chart.

--- www/js/controller_charts.js
+++ www/js/controller_charts.js
@@ -5,7 +5,7 @@ angular.module('starter.controller_charts', [])
 
 	_scope_charts = $scope;
 
-	$scope.data2 = {
+	$scope.dataTheme = {
 		theme: ProjectService.getTheme()
 	};
 
@@ -18,6 +18,50 @@ angular.module('starter.controller_charts', [])
 	$scope.numTask  = 0;
 	$scope.completed= 0;
 
+
+	$scope.data1    = [];
+	$scope.data2    = [];
+	$scope.series   = [];
+
+
+	$scope.projects.forEach(function(p) {
+		$scope.data1.push({
+			projectName: p.title,
+			numTask    : p.tasks.length
+		});
+	});
+
+	$("#chartContainer1").dxPieChart({
+			dataSource: $scope.data1,
+			palette: "Soft Pastel",
+			legend: {
+					horizontalAlignment: "right",
+					verticalAlignment: "top",
+					margin: 0
+			},
+			pointClick: function(point) {
+					point.select();
+			},
+			series: [{
+				type: "doughnut",
+				argumentField: "projectName",
+				valueField: "numTask",
+				hoverStyle: {
+					color: "#ffd700" 
+				},
+				label: {
+            visible: true,
+            connector: {
+                visible: true,
+                width: 1
+            },
+            customizeText: function(arg) {
+                return arg.valueText + " ( " + arg.percentText + ")";
+            }
+        }
+			}]
+	});
+
 	$scope.projects.forEach(function(p) {
 		$scope.numTask = $scope.numTask + p.tasks.length;
 		p.tasks.forEach(function(t) {
@@ -26,8 +70,6 @@ angular.module('starter.controller_charts', [])
 			}
 		});
 	});
-	$scope.data     = [];
-	$scope.series   = [];
 
 	// Get last 7 days.
 	for (var i = 6; i >= 0; i--) {
@@ -39,13 +81,13 @@ angular.module('starter.controller_charts', [])
 		dd = (dd < 10) ? '0' + dd : dd;
 		mm = (mm < 10) ? '0' + mm : mm;
 		
-		$scope.data.push({
+		$scope.data2.push({
 			date: mm + '/' + dd
 		});
 	}
 
 	// Initialize projects in each day
-	$scope.data.forEach(function(day) {
+	$scope.data2.forEach(function(day) {
 		for (var i = 0; i < $scope.projects.length; i++) {
 			day[$scope.projects[i].title] = 0;
 		}
@@ -54,8 +96,7 @@ angular.module('starter.controller_charts', [])
 	// Assign number of tasks for each project
 	$scope.history.forEach(function(h) {
 		if (h.type === 1) {
-			$scope.data.forEach(function(d) {
-				// console.log(h, d);
+			$scope.data2.forEach(function(d) {
 				if (d.date === h.date) {
 					d[h.project] ++;
 				}
@@ -71,17 +112,17 @@ angular.module('starter.controller_charts', [])
 		});
 	});
 
-	console.log($scope.data);
+	console.log($scope.data2);
 
-	(function () {
-		$("#chartContainer").dxChart({
-			dataSource: $scope.data,
+	$(function () {
+		$("#chartContainer2").dxChart({
+			dataSource: $scope.data2,
+			palette: "Soft Pastel",
 			commonSeriesSettings: {
 				argumentField: "date",
 				type: "stackedLine"
 			},
 			series: $scope.series,
-			title: "New tasks for last week",
 			legend: {
 				verticalAlignment: "bottom",
 				horizontalAlignment: "center"
@@ -96,6 +137,15 @@ angular.module('starter.controller_charts', [])
 					visible: true
 				}
 			},
+			argumentAxis: {
+				label: {
+					visible: false
+				}
+			},
 		});
-	})();
+	});
+
+	
+
+
 });
